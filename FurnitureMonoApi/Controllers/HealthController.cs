16c2629111d6ad947d1707a26133e1ba9a6361b1
renaldi99 <renaldisimamora67@gmail.com>
2024-05-25 19:24:10 +0700

    first commit repo

---
+++ FurnitureMonoApi/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FurnitureMonoApi.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult GetHealth()
+    {
+        return Ok("Oke Health");
+    }
+}
+
